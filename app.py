9ae01431102358ef182f838d9ff9f7a100938a2c
Ian Dao <ibdao921@gmail.com>
2022-06-09 12:32:19 -0700

    can visit first question

--- app.py
+++ app.py
@@ -8,8 +8,26 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-@app.get("/home")
-def generate_start_page():
-    return render_template("survey_start.html")
+responses =[]
 
 @app.get("/begin")
+def generate_start_page():
+    """Generates landing page"""
+    
+    return render_template(
+        "survey_start.html", 
+        title= survey.title, 
+        instructions= survey.instructions)
+
+@app.post("/begin")
+def handle_post_request():
+    return redirect ('/questions/0')
+
+@app.get("/questions/<question_number>")
+def generate_questions(question_number):
+    return render_template(
+        "question.html",
+        question = survey.questions[int(question_number)]) 
+
+
+    
